8e7125083112480f9f52076f82fb1f1966ef9113
Lal Angdembe <lalangdembe@MacBook-Pro.local>
2021-12-19 14:29:15 -0800

    spring boot maven project created

---
+++ spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/SpringBootRestItemApplication.java
@@ -0,0 +1,13 @@
+package com.lal.springbootrestitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestItemApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rest-item/src/test/java/com/lal/springbootrestitem/SpringBootRestItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lal.springbootrestitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
