64574774eddf436c42af9fc1569155c3da30bd66
Lal Angdembe <lalangdembe@MacBook-Pro.local>
2021-12-20 14:07:56 -0800

    update

--- spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/controller/ItemController.java
+++ spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/controller/ItemController.java
@@ -1,28 +1,30 @@
 package com.lal.springbootrestitem.controller;
 
 
-
-import java.util.Date;
+import java.util.List;
 
 import com.lal.springbootrestitem.entity.Item;
+import com.lal.springbootrestitem.service.ItemService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
+ 
+ @Autowired  
+ ItemService itemService;
 
-    
     @GetMapping("/items")
-    public Item getItem(){
+    public List<Item> getItems(){
+         return itemService.getAllItems();
+    }
 
-        Item item = new Item();
-        item.setId(1);
-        item.setName("Table");
-        item.setDescription("Wooden tea table");
-        item.setCreated(new Date());
-        item.setUpdated(new Date());
-        return item;
+    @GetMapping("/items/{id}")
+    public Item getItem(@PathVariable ("id") int id){
+        return itemService.getAnItem(id);
     }
     
 }
