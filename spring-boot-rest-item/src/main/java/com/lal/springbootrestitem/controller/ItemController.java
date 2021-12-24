5f4b5507d574b905b5b1f7ef23aa527679b63ecd
Lal Angdembe <lalangdembe@MacBook-Pro.local>
2021-12-23 18:33:42 -0800

    CRUD

--- spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/controller/ItemController.java
+++ spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/controller/ItemController.java
@@ -7,17 +7,21 @@ import com.lal.springbootrestitem.entity.Item;
 import com.lal.springbootrestitem.service.ItemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
  
  @Autowired  
- ItemService itemService;
+ private ItemService itemService;
 
-    @GetMapping("/items")
+    @GetMapping({"/","/items"})
     public List<Item> getItems(){
          return itemService.getAllItems();
     }
@@ -27,4 +31,22 @@ public class ItemController {
         return itemService.getAnItem(id);
     }
     
+    @PostMapping("/addItem")
+    public Item addItem(@RequestBody Item item)
+    {
+        return itemService.addItem(item);
+    }
+
+    @DeleteMapping("/items/{itemId}")
+    public void deleteItem(@PathVariable("itemId") int id){
+        itemService.deleteItem(id);
+    }
+
+    @PutMapping("/items/{itemId}")
+    public Item updateItem(@RequestBody Item item, @PathVariable("itemId") int bookId){
+        itemService.updateItem(item, bookId);
+        return item;
+    }
+
+
 }
--- spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/service/ItemService.java
+++ spring-boot-rest-item/src/main/java/com/lal/springbootrestitem/service/ItemService.java
@@ -3,6 +3,7 @@ package com.lal.springbootrestitem.service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.lal.springbootrestitem.entity.Item;
 
@@ -27,6 +28,35 @@ public class ItemService {
         Item item = null;
         item = items.stream().filter( x -> x.getId() == id).findFirst().get();
         return item;
+    }
+
+    public Item addItem(Item item){
+        items.add(item);
+        return item;
+    }
+
+    public void deleteItem(int id) {
+        // items = items.stream().filter(item ->{
+        //     if(item.getId() != id){
+        //         return true;
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }).collect(Collectors.toList());
+
+        items = items.stream().filter((item) -> item.getId() != id).collect(Collectors.toList());
+
+    }
+
+    public void updateItem(Item item, int bookId) {
+        items = items.stream().map(itm-> {if(itm.getId() == bookId){
+            itm.setName(item.getName());
+            itm.setDescription(item.getDescription());
+            itm.setUpdated(item.getUpdated());
+        }
+        return itm;
+    }).collect(Collectors.toList());
     }
 
 }
