a83bd87402e7983e69551d153110bb48af348104
Lal Angdembe <lalangdembe@MacBook-Pro.local>
2021-12-25 12:41:19 -0800

    First Commmit

---
+++ src/test/java/com/aza/bootapirestjpa/BootApiRestJpaApplicationTests.java
@@ -0,0 +1,18 @@
+package com.aza.bootapirestjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApiRestJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	
+	
+	
+	
+	
+	}
+
+}
